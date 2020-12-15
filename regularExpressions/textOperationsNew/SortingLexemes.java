90c74d77c5d29063f924d194086dfeced5680568
DR972 <74292696+DR972@users.noreply.github.com>
2020-12-15 19:35:46 +0300

    Add files via upload

---
+++ regularExpressions/textOperationsNew/SortingLexemes.java
@@ -0,0 +1,57 @@
+package by.epam.learn.main;
+
+public class SortingLexemes extends Splitting {
+    private final String[] paragraphs;
+    private final char letter;
+
+    public SortingLexemes(String text, char letter) {
+        this.letter = letter;
+        paragraphs = splitParagraphs(text);
+    }
+
+    public int countLetters(String word) {
+        int sum = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == letter) {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+    public StringBuilder sortWords(String[] words) {
+        StringBuilder sentences = new StringBuilder();
+        for (int i = 0; i < words.length - 1; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (countLetters(words[j]) > countLetters(words[i])) {
+                    String temp = words[i];
+                    words[i] = words[j];
+                    words[j] = temp;
+                }
+                if (countLetters(words[j]) == countLetters(words[i])) {
+                    if (words[i].compareToIgnoreCase(words[j]) > 0) {
+                        String temp = words[i];
+                        words[i] = words[j];
+                        words[j] = temp;
+                    }
+                }
+            }
+            sentences.append(words[i]).append(" ");
+        }
+        sentences.append(words[words.length - 1]).append(".");
+        return sentences;
+    }
+
+    public String sortText() {
+        StringBuilder newText = new StringBuilder();
+        for (String paragraph : paragraphs) {
+            String[] sentences = splitSentences(paragraph);
+            for (String sentence : sentences) {
+                String[] words = splitWords(sentence);
+                newText.append(sortWords(words)).append(" ");
+            }
+            newText.append("\n");
+        }
+        return newText.toString();
+    }
+}
---
+++ regularExpressions/textOperationsNew/SortingParagraphs.java
@@ -0,0 +1,35 @@
+package by.epam.learn.main;
+
+public class SortingParagraphs extends Splitting {
+    static public int maxSentence;
+    private final String[] paragraphs;
+
+    public SortingParagraphs(String text) {
+        paragraphs = splitParagraphs(text);
+    }
+
+    public int[] sortSentences() {
+        int[] numberOfSentences = new int[paragraphs.length];
+        for (int i = 0; i < paragraphs.length; i++) {
+            String[] sentences = splitSentences(paragraphs[i]);
+            numberOfSentences[i] = sentences.length;
+            if (maxSentence < numberOfSentences[i]) {
+                maxSentence = numberOfSentences[i];
+            }
+        }
+        return numberOfSentences;
+    }
+
+    public String sortParagraphs() {
+        StringBuilder newParagraphs = new StringBuilder();
+        sortSentences();
+        for (int i = 1; i <= maxSentence; i++) {
+            for (int j = 0; j < paragraphs.length; j++) {
+                if (sortSentences()[j] == i) {
+                    newParagraphs.append(paragraphs[j]).append("\n");
+                }
+            }
+        }
+        return newParagraphs.toString();
+    }
+}
