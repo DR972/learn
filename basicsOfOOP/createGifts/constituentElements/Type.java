74a3d75e1abac394cf2c70e12472a1295dc61276
DR972 <74292696+DR972@users.noreply.github.com>
2021-08-09 19:08:01 +0300

    Delete basicsOfOOP directory

--- basicsOfOOP/createGifts/constituentElements/Type.java
+++
@@ -1,7 +0,0 @@
-package by.epam.learn.main.modul5.createGifts.constituentElements;
-
-public interface Type {
-    String getName();
-    int getWeight();
-    double getPrice();
-}
--- basicsOfOOP/createGiftsNew/model/Type.java
+++
@@ -1,7 +0,0 @@
-package by.epam.learn.main.modul5.createGiftsNew.model;
-
-public interface Type {
-    String getName();
-    int getWeight();
-    double getPrice();
-}
